7307fa5dc733d4aa839a587445c0ed8cf5003e1b
jraulcr <jraulcr@gmail.com>
2020-02-09 19:00:09 +0100

    Inicio curso JQuery 01. 09/02/2020

---
+++ aprendiendo-jquery/js/01-selectores.js
@@ -0,0 +1,16 @@
+
+
+$(document).ready(function(){
+    //Selector de ID
+
+    var rojo = $('#rojo').css('background','red')
+                        .css('color','white');
+
+    $('#amarillo').css('background','yellow')
+                        .css('color','green');
+
+    $('#verde').css('background','green')
+                        .css('color','yellow');
+
+    console.log(rojo);
+});
