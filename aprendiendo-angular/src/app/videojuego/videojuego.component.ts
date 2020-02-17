ec1dd9b3f86dcd9bf89f0cbc6597b18cb304c991
jraulcr <jraulcr@gmail.com>
2020-02-17 12:37:44 +0100

    Inicio curso Angular 17/02/2020

---
+++ aprendiendo-angular/src/app/videojuego/videojuego.component.ts
@@ -0,0 +1,38 @@
+import { Component, OnInit, DoCheck, OnDestroy } from '@angular/core';
+
+@Component({
+  selector: 'videojuego',
+  templateUrl: './videojuego.component.html'
+})
+
+export class VideojuegoComponent implements OnInit, DoCheck, OnDestroy{
+  public titulo: string;
+  public listado: string;
+
+  constructor() {
+    this.titulo = 'Componente de Videojuegos';
+    this.listado = 'Listado de los juegos mas populares';
+    console.log('Se ha cargado el componente: videojuego.component.ts')
+  }
+
+
+
+ngOnInit(){
+  console.log('OnInit ejecutado!!!!!');
+}
+
+ngDoCheck(){
+  console.log("DoCheck Ejecutado...!!");
+}
+
+ngOnDestroy(){
+  console.log("OnDestroy Ejecutado...!!");
+}
+
+cambiarTitulo(){
+  this.titulo = "Nuevo titulo del componente";
+}
+
+}
+
+
---
+++ aprendiendo-angular/src/app/zapatillas/zapatillas.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'zapatillas',
+  templateUrl: './zapatillas.component.html'
+})
+
+export class ZapatillasComponent {
+  public titulo: string = "Componente de zapatillas";
+
+}
+
